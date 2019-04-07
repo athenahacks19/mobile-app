985e9923843d18e774ff3c71a183ab8d092428c0
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-07 02:36:49 -0700

    Get sentiment analysis and display score nicely

--- src/app/patient-tab3/patient-tab3.page.ts
+++ src/app/patient-tab3/patient-tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-patient-tab3',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-tab3.page.scss'],
 })
 export class PatientTab3Page implements OnInit {
+  myText = new FormControl('');
+  lastScore: number;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+  }
+
+  submit() {
+    this.dataService.analyzeText(this.myText.value)
+    .then((score: number) => {
+      this.lastScore = score;
+    });
   }
 
 }
