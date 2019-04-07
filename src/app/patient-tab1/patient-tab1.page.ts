ee0105f58bb1e5394c5cdea73bc92298583a5066
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-07 12:11:25 -0700

    Get user on patient tab 1

--- src/app/patient-tab1/patient-tab1.page.ts
+++ src/app/patient-tab1/patient-tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { User } from '../interfaces';
 
 @Component({
   selector: 'app-patient-tab1',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-tab1.page.scss'],
 })
 export class PatientTab1Page implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.get('1234')
+    .then((user: User) => {
+      this.user = user;
+    });
   }
 
 }
