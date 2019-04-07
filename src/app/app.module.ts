4c4fe4c2dee80841e25e9314bfb9e4185b93a4ff
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-07 08:15:38 -0700

    Analyze, save, and display images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { Camera} from '@ionic-native/camera/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { AppComponent } from './app.component';
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     SpeechRecognition,
+    Camera,
   ],
   bootstrap: [AppComponent]
 })
