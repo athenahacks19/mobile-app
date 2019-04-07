d0406ddab4ed690c4bb6f4249633fb4d841be99a
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-07 00:40:26 -0700

    Add Azure map

--- src/app/parent-tab1/parent-tab1.page.ts
+++ src/app/parent-tab1/parent-tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as atlas from 'azure-maps-control';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-parent-tab1',
@@ -10,6 +12,16 @@ export class ParentTab1Page implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = new atlas.Map('myMap', {
+      center: [-122.33, 47.6],
+      zoom: 12,
+      authOptions: {
+        authType: 'subscriptionKey' as atlas.AuthenticationType,
+        subscriptionKey: environment.mapKey
+      },
+      autoResize: true
+    });
+    map.resize(1000, 300);
   }
 
 }
